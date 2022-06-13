930be34582e49b22a12f68d2e929cd9369a4f6a3
Alexandr <samsoncik1@gmail.com>
2022-06-13 18:25:24 +0300

    HW3

---
+++ src/HomeWork_13_06_2022_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HomeWork_13_06_2022_3 {
+    public static void main(String[] args) {
+        //Создайте программу, определяющую является число чётным или нечётным.
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите любое число, а мы скажем является ли оно чётным или нет: ");
+        int x = in.nextInt();
+        if (x % 2 == 0) System.out.println("Число " + x + " является чётным числом");
+        else System.out.println("Число " + x + " является нечётным числом");
+
+
+    }
+}
